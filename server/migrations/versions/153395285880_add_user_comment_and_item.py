5462b390438be1277cbc978fb0f077db357d2710
Brandi Burnell <brandiburnell6@gmail.com>
2024-07-27 12:40:17 -0600

    add user, comment, and item models

---
+++ server/migrations/versions/153395285880_add_user_comment_and_item.py
@@ -0,0 +1,24 @@
+"""add user, comment, and item
+
+Revision ID: 153395285880
+Revises: 
+Create Date: 2024-07-27 12:34:09.450555
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '153395285880'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -1,6 +1,66 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    serialize_rules = ('-_password_hash',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String)
+
+    @hybrid_property
+    def password_hash(self):
+        raise Exception('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+
+    # reviews = db.relationship('Review', backref='user')
+
+    def __repr__(self):
+        return f'<User {self.id}, {self.username}>'
+
+class Item(db.Model, SerializerMixin):
+    __tablename__ = 'items'
+
+    # serialize_rules = ('-reviews.book', '-reviews.user')
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    year_purchased = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # reviews = db.relationship('Review', backref='book')
+
+    def __repr__(self):
+        return f'<Item {self.id}, {self.name}: {self.description}, {self.year_purchased}, {self.image_url}, {self.owner_id}>'
+    
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    def __repr__(self):
+        return f'<Comment {self.id}, {self.subject}: {self.description}>'
